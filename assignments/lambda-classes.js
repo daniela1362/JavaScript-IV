5f5b1e10a9776a55aa9f019b1311f89fa70cb809
daniela-arias-ordonez <daniela.arias2000@gmail.com>
2019-06-13 17:41:24 -0500

    Added person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.hometown;
+    }
+    speak(){
+        return `Hello, my name is {$this.name}, I am from {$this.location}`;
+    }
+}
